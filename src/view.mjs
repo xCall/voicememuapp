33a76b0d67831b2d6c39784c07f9026597eb4b8e
xCall <carlosfaita99@gmail.com>
2022-03-08 23:57:00 -0300

    Project voice memu

---
+++ src/view.mjs
@@ -0,0 +1,23 @@
+export default class View {
+  constructor() {
+    this.btnStart = document.getElementById('btnStart');
+    this.btnStop = document.getElementById('btnStop');
+    this.audio = document.getElementById('audio');
+  }
+  onRecordClick(command) {
+    return () => {
+      command()
+    }
+  }
+  onStopRecordClick(command) {
+    return () => {
+      command()
+    }
+  }
+  configureStartRecordingButton(command) {
+    this.btnStart.addEventListener('click', this.onRecordClick(command));
+  }
+  configureStopRecordingButton(command) {
+    this.btnStop.addEventListener('click', this.onStopRecordClick(command));
+  }
+}
